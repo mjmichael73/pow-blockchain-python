32913056ecc4fa07dfb52c7d608938a723c4a078
Mojtaba Michael <mojimich2015@gmail.com>
2025-12-08 23:29:17 +0300

    Added create_block method

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -1,3 +1,4 @@
+from time import time
 from flask import Flask, render_template
 
 
@@ -5,6 +6,31 @@ class Blockchain:
     def __init__(self):
         self.transactions = []
         self.chain = []
+        # Create the genesis block
+        self.create_block(
+            nonce=0,
+            previous_hash="00",
+        )
+
+    def create_block(self, nonce, previous_hash):
+        """
+        Add a block of transaction to the blockchain
+        :param nonce:
+        :param previous_hash:
+        :return:
+        """
+        block = {
+            "block_number": len(self.chain) + 1,
+            "timestamp": time(),
+            "transactions": self.transactions,
+            "nonce": nonce,
+            "previous_hash": previous_hash,
+        }
+        # Reset the current list of transactions
+        self.transactions = []
+
+        # Add the new block to the chain
+        self.chain.append(block)
 
 
 # Instantiate the Blockchain
